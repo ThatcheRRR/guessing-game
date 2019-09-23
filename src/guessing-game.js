3fe08f33fb20a5ee13e461b5b93cd8f59c11777a
ThatcheRRR <pavelosipov777@mail.ru>
2019-09-23 21:20:28 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let mid = 0;
+        while (this.min <= this.max) {
+            mid = Math.floor((this.max + this.min) / 2);
+        }
+        return mid;
     }
 
     lower() {
-
+        this.mid = this.max - 1;
     }
 
     greater() {
-
+        this.mid = this.min + 1;
     }
 }
 
